3da9f8b4c7bad7ce7b6c39beaf2c9ef12a06ca5b
difico123 <ducluong0915@gmail.com>
2022-04-24 01:22:56 +0700

    fix

--- frontend/screens/Home.jsx
+++ frontend/screens/Home.jsx
@@ -1,14 +1,4 @@
-import {
-  View,
-  Text,
-  StyleSheet,
-  FlatList,
-  TouchableOpacity,
-  Image,
-  ScrollView,
-  VirtualizedList,
-  RefreshControl,
-} from "react-native";
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Image, ScrollView, VirtualizedList, RefreshControl } from "react-native";
 import React, { useState, useEffect } from "react";
 import { Icon } from "react-native-elements";
 import { useSelector, useDispatch } from "react-redux";
@@ -26,156 +16,134 @@ import { toEventCollection } from "../resources/events/EventResource";
 import { wait } from "../helpers/helpers";
 
 const Home = ({ navigation }) => {
-  const [refreshing, setRefreshing] = React.useState(false);
-  const onRefresh = React.useCallback(() => {
-    setRefreshing(true);
-    wait(2000).then(() => setRefreshing(false));
-  }, []);
+    const [refreshing, setRefreshing] = React.useState(false);
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true);
+        wait(2000).then(() => setRefreshing(false));
+    }, []);
 
-  const [isToggleNav, setToggleNav] = useState(false);
-  const [searchEvent, setSearchEvent] = useState("");
-  const [allEvents, setAllEvents] = useState([]);
+    const [isToggleNav, setToggleNav] = useState(false);
+    const [searchEvent, setSearchEvent] = useState("");
+    const [allEvents, setAllEvents] = useState([]);
 
-  const auth = useSelector((state) => state.authReducers.auth);
-  const dispatch = useDispatch();
-  const nav = useNavigation();
+    const auth = useSelector((state) => state.authReducers.auth);
+    const dispatch = useDispatch();
+    const nav = useNavigation();
 
-  useEffect(() => {
-    if (isToggleNav) {
-      navigation.setOptions({
-        tabBarLabel: "Home",
-        tabBarStyle: { display: "none" },
-      });
-    } else {
-      navigation.setOptions({
-        tabBarLabel: "Home",
-        tabBarStyle: {
-          display: "flex",
-          position: "absolute",
-          bottom: 10,
-          left: 10,
-          right: 10,
-          elevation: 1,
-          backgroundColor: "#FFFFFF",
-          borderRadius: 10,
-          height: 70,
-          paddingBottom: 10,
-          paddingTop: 5,
-          borderWidth: 1,
-          borderColor: BORDER_COLOR,
-        },
-      });
-    }
-  }, [isToggleNav]);
+    console.log("event", allEvents);
 
-  useEffect(() => {
-    // console.log(auth.user);
-  }, []);
-
-  useEffect(() => {
-    const getAll = async () => {
-      setAllEvents(await EventService.getEvents(auth.token));
-    };
-    getAll();
-  }, [refreshing]);
-
-  const getItemCount = allEvents.length;
-  const showEventList = () => {
-    nav.navigate("EventList");
-  };
-
-  const EventHotList = allEvents.map((item, index) => (
-    <EventItemHot
-      item={item}
-      key={index}
-      onPress={() => goToDetail(item.id)}
-      onFresh={refreshing}
-    />
-  ));
-
-  const goToDetail = (id) => {
-    nav.navigate("DetailEvent", { id });
-  };
-
-  const [upcomingEvents, setUpcomingEvents] = useState([]);
-  useEffect(() => {
-    const updateUpcomingEvent = async () => {
-      const record = await EventService.getEvents(auth.token, {
-        start_at: new Date().getTime(),
-      });
-      setUpcomingEvents(await toEventCollection(record));
-    };
-    updateUpcomingEvent();
-  }, [refreshing]);
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <SearchBar
-          placeholder="Tìm kiếm"
-          setToggleNav={setToggleNav}
-          setValue={setSearchEvent}
-          value={searchEvent}
-        />
-      </View>
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={styles.scrollView}
-        refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+    useEffect(() => {
+        if (isToggleNav) {
+            navigation.setOptions({
+                tabBarLabel: "Home",
+                tabBarStyle: { display: "none" },
+            });
+        } else {
+            navigation.setOptions({
+                tabBarLabel: "Home",
+                tabBarStyle: {
+                    display: "flex",
+                    position: "absolute",
+                    bottom: 10,
+                    left: 10,
+                    right: 10,
+                    elevation: 1,
+                    backgroundColor: "#FFFFFF",
+                    borderRadius: 10,
+                    height: 70,
+                    paddingBottom: 10,
+                    paddingTop: 5,
+                    borderWidth: 1,
+                    borderColor: BORDER_COLOR,
+                },
+            });
         }
-      >
-        <View style={styles.eventContainer}>
-          <View style={[CommonStyle.spaceBetween]}>
-            <TouchableOpacity>
-              <Text style={styles.title}>Sự kiện sắp diễn ra</Text>
-            </TouchableOpacity>
-            {/* <TouchableOpacity onPress={showEventList}>
+    }, [isToggleNav]);
+
+    useEffect(() => {
+        // console.log(auth.user);
+    }, []);
+
+    useEffect(() => {
+        const getAll = async () => {
+            setAllEvents(await EventService.getEvents(auth.token));
+        };
+        getAll();
+    }, [refreshing]);
+
+    const getItemCount = allEvents.length;
+    const showEventList = () => {
+        nav.navigate("EventList");
+    };
+
+    const EventHotList = allEvents.map((item, index) => <EventItemHot item={item} key={index} onPress={() => goToDetail(item.id)} onFresh={refreshing} />);
+
+    const goToDetail = (id) => {
+        nav.navigate("DetailEvent", { id });
+    };
+
+    const [upcomingEvents, setUpcomingEvents] = useState([]);
+    useEffect(() => {
+        const updateUpcomingEvent = async () => {
+            const record = await EventService.getEvents(auth.token, {
+                start_at: new Date().getTime(),
+            });
+            setUpcomingEvents(await toEventCollection(record));
+        };
+        updateUpcomingEvent();
+    }, [refreshing]);
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <SearchBar placeholder="Tìm kiếm" setToggleNav={setToggleNav} setValue={setSearchEvent} value={searchEvent} />
+            </View>
+            <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.scrollView} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+                <View style={styles.eventContainer}>
+                    <View style={[CommonStyle.spaceBetween]}>
+                        <TouchableOpacity>
+                            <Text style={styles.title}>Sự kiện sắp diễn ra</Text>
+                        </TouchableOpacity>
+                        {/* <TouchableOpacity onPress={showEventList}>
               <Text style={styles.title}>Xem tất cả</Text>
             </TouchableOpacity> */}
-          </View>
+                    </View>
 
-          <FlatList
-            showsHorizontalScrollIndicator={false}
-            data={upcomingEvents}
-            horizontal
-            keyExtractor={(item) => item.id}
-            renderItem={({ item, index }) => (
-              <EventItemIncomming
-                item={item}
-                key={index}
-                onPress={() => goToDetail(item.id)}
-              />
-            )}
-          />
+                    <FlatList
+                        showsHorizontalScrollIndicator={false}
+                        data={upcomingEvents}
+                        horizontal
+                        keyExtractor={(item) => item.id}
+                        renderItem={({ item, index }) => <EventItemIncomming item={item} key={index} onPress={() => goToDetail(item.id)} />}
+                    />
+                </View>
+            </ScrollView>
         </View>
-      </ScrollView>
-    </View>
-  );
+    );
 };
 
 export default Home;
 
 const styles = StyleSheet.create({
-  scroll: {
-    overflow: "hidden",
-    height: 10000,
-  },
-  container: {},
-  header: {
-    marginTop: 10,
-    marginBottom: 5,
-  },
-  eventItem: {},
-  title: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  icon: {},
-  eventContainer: {
-    padding: 15,
-  },
-  hotEventTitle: {
-    marginBottom: 10,
-  },
+    scroll: {
+        overflow: "hidden",
+        height: 10000,
+    },
+    container: {},
+    header: {
+        marginTop: 10,
+        marginBottom: 5,
+    },
+    eventItem: {},
+    title: {
+        fontSize: 16,
+        fontWeight: "bold",
+    },
+    icon: {},
+    eventContainer: {
+        padding: 15,
+    },
+    hotEventTitle: {
+        marginBottom: 10,
+    },
 });
